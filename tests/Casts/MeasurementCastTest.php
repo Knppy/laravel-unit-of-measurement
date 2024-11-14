3c4d047af9f4eb8b84ea9ca3658303c6be0417e7
michaelbeers <michaelbeers@users.noreply.github.com>
2024-11-14 14:35:13 +0000

    Fix styling

--- tests/Casts/MeasurementCastTest.php
+++ tests/Casts/MeasurementCastTest.php
@@ -4,24 +4,24 @@ use Illuminate\Database\Eloquent\Model;
 use Knppy\UnitOfMeasurement\Casts\MeasurementCast;
 use Knppy\UnitOfMeasurement\Measurement;
 
-beforeEach(function() {
+beforeEach(function () {
     $this->model = $this->getMockBuilder(Model::class)
         ->disableOriginalConstructor()
         ->getMock();
 });
 
-it ('can get a measurement from a string', function () {
-    $this->assertEquals(new Measurement('gram'), (new MeasurementCast())->get($this->model, '', 'gram', []));
+it('can get a measurement from a string', function () {
+    $this->assertEquals(new Measurement('gram'), (new MeasurementCast)->get($this->model, '', 'gram', []));
 });
 
-it ('can set a measurement to a string', function () {
-    $this->assertEquals('gram', (new MeasurementCast())->set($this->model, '', new Measurement('gram'), []));
+it('can set a measurement to a string', function () {
+    $this->assertEquals('gram', (new MeasurementCast)->set($this->model, '', new Measurement('gram'), []));
 });
 
-it ('throws an exception if the get value is not a string', function() {
-    (new MeasurementCast())->get($this->model, '', 1, []);
+it('throws an exception if the get value is not a string', function () {
+    (new MeasurementCast)->get($this->model, '', 1, []);
 })->expectException(UnexpectedValueException::class);
 
-it ('throws an exception if the set value is not a measurement', function () {
-    (new MeasurementCast())->set($this->model, '', 'gram', []);
+it('throws an exception if the set value is not a measurement', function () {
+    (new MeasurementCast)->set($this->model, '', 'gram', []);
 })->expectException(UnexpectedValueException::class);
